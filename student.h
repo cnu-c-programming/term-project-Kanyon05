7645f8ca88d4ab554096cb15b5132a8fd158a3e8
Kanyon05 <windtree01@gmail.com>
2026-05-31 21:20:33 +0900

    add plan function to student.h

--- student.h
+++ student.h
@@ -7,4 +7,14 @@ typedef struct Student {
     struct Student* next;
 } Student;
 
+void add_student(Student** head, int id, const char* name, int score);
+void delete_student(Student** head, int id);
+void update_student (Student* head, int id, int score);
+Student* find_student(Student* head, int id);
+
+void list_students(Student* head);
+void print_stats(Student* head);
+
+void free_all_students(Student** head);
+
 #endif
